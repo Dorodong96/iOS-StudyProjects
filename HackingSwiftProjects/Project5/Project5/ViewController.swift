6e846c2ac6f79c9afd053b73895b4883641aad32
Dongkyu Kim <dk143496@gmail.com>
2022-07-20 00:40:22 +0900

    feat: Word Scarmble Check - add UIAlertController (alert)

--- HackingSwiftProjects/Project5/Project5/ViewController.swift
+++ HackingSwiftProjects/Project5/Project5/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
     
@@ -50,6 +50,7 @@ class ViewController: UITableViewController {
 
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
         
         let submitAction = UIAlertAction(title: "Submit", style: .default) {
             // closure input
@@ -63,8 +64,71 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    
     func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    // with -> 애니메이션
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return // 메소드 탈출
+                } else {
+                    errorTitle = "Word not recogized"
+                    errorMessage = "You can't just make them up, you know?"
+                }
+            } else {
+                errorTitle = "Word already used"
+                errorMessage = "Be more original"
+            }
+        } else {
+            guard let title = title else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title.lowercased())"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    
+    func isPossible(word: String) -> Bool {
+        // 가능한 단어라면 추가 (제시된 단어에서 나올 수 있는 단어)
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    
+    func isOriginal(word: String) -> Bool {
+        // 단어 중복 체크하기
+        return !usedWords.contains(word)
+    }
+    
+    
+    func isReal(word: String) -> Bool {
+        // 문법에 맞는 단어인지 (실제로 존제하는 단어인지 체크)
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
     }
 }
 
