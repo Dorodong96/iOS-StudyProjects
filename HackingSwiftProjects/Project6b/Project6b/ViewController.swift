35e386d05d8c74475c1c4198d506a8454866fe85
Dongkyu Kim <dk143496@gmail.com>
2022-07-23 09:56:59 +0900

    feat: add anchor constraints related to main view anchor in ViewController

--- HackingSwiftProjects/Project6b/Project6b/ViewController.swift
+++ HackingSwiftProjects/Project6b/Project6b/ViewController.swift
@@ -49,22 +49,43 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = [
-            "label1": label1,
-            "label2": label2,
-            "label3": label3,
-            "label4": label4,
-            "label5": label5
-        ]
-        
-        for label in viewsDictionary.keys {
-            // 여기서 view는 Main View
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewsDictionary))
-        }
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", metrics: nil, views: viewsDictionary))
-    }
+//        let viewsDictionary = [
+//            "label1": label1,
+//            "label2": label2,
+//            "label3": label3,
+//            "label4": label4,
+//            "label5": label5
+//        ]
+//
+//        for label in viewsDictionary.keys {
+//            // 여기서 view는 Main View
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewsDictionary))
+//        }
+//
+//        // metrics로 파라미터화 해서 전달
+//        let metrics = ["labelHeight": 88]
+//
+//        // ==80 : 높이, (>=10) : space
+//        // @999, ... : Contstraint Priority (default는 1000이고 높을수록 중요도 높음, 따라서 낮으면 fit 하게 만듦
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", metrics: metrics, views: viewsDictionary))
+//    }
     
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            //label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            previous = label
+        }
+    }
     
 }
 
