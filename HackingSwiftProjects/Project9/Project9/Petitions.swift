b2804c7a39c589f4b2e23853a36beb6fc10db0ec
Dongkyu Kim <dk143496@gmail.com>
2022-08-03 01:24:24 +0900

    feat: add GCD (separated main & background thread)

---
+++ HackingSwiftProjects/Project9/Project9/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by DongKyu Kim on 2022/07/26.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
---
+++ HackingSwiftProjects/Project9/Project9/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by DongKyu Kim on 2022/07/26.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions: [Petition] = []
+    var filteredPetitions: [Petition] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredit))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filter))
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            // 해당 url이 타당한지 검토
+            if let url = URL(string: urlString) {
+                // contentsOf로 Data Object를 만들어 항목을 반환 (하지만 인터넷 문제 등의 원인으로 에러 발생 가능 -> try?)
+                if let data = try? Data(contentsOf: url) {
+                    // WE ARE OK TO PARSE
+                    self.parse(json: data)
+                    return
+                }
+            }
+            self.showError()
+        }
+        
+        
+    }
+    
+    
+    func showError() {
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
+    
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        // Petition 타입으로 json 파일에서 추출하기
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filteredPetitions = petitions
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    @objc func showCredit() {
+        let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    // filter alert controller
+    @objc func filter() {
+        let ac = UIAlertController(title: "Filter", message: "Write the word what you want to filter", preferredStyle: .alert)
+        let filterAction = UIAlertAction(title: "Done", style: .default) { (filter) in
+            self.filteredPetitions = []
+            self.filterWord(ac.textFields?[0].text)
+        }
+        ac.addTextField()
+        ac.addAction(filterAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    // petition의 단어 필터링
+    func filterWord(_ word: String?) {
+        if let word = word {
+            for petition in petitions {
+                if petition.title.contains(word) {
+                    filteredPetitions.append(petition)
+                    tableView.reloadData()
+                }
+            }
+        } else {
+            print("No Results")
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = filteredPetitions[indexPath.row]
+        
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = filteredPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
