7bd38a9948e99a13c92d2e0edc916874b8163daf
Dongkyu Kim <dk143496@gmail.com>
2022-08-14 16:35:08 +0900

    feat: UserDefaults with  Codable Protocols for saving & converting data

---
+++ HackingSwiftProjects/Project12b/Project12b/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by DongKyu Kim on 2022/08/08.
+//
+
+import UIKit
+
+class Person: NSObject, Codable {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
