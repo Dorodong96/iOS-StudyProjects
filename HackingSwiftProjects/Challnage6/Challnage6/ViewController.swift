cb42e93d0d8ab82f96bf730ef0531856f841470b
Dongkyu Kim <dk143496@gmail.com>
2022-09-14 11:08:49 +0900

    add: Challange6 files

---
+++ HackingSwiftProjects/Challnage6/Challnage6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challnage6
+//
+//  Created by DongKyu Kim on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
