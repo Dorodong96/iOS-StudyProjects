dd7a745be6e99e70b1a4488f8e62793bab70f38d
Dongkyu Kim <dk143496@gmail.com>
2022-10-29 23:52:42 +0900

    [Fix] RxSwiftPractice 수정

---
+++ FastCampus/RxSwiftPractice/RxSwiftPractice/ObservablePractice.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+import RxSwift
+
+print("---Just---")
+Observable<Int>.just(1)
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("---Of1---")
+Observable<Int>.of(1, 2, 3, 4, 5)
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("---Of2---")
+Observable.of([1, 2, 3, 4, 5])
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("---From---")
+Observable.from([1, 2, 3, 4, 5])
+    .subscribe(onNext:  {
+        print($0)
+    })
