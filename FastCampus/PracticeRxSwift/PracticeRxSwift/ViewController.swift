584a62245ca5c74fa3506027a25f91a084689a13
Dongkyu Kim <dk143496@gmail.com>
2022-10-31 08:55:02 +0900

    feat: Subject types

--- FastCampus/PracticeRxSwift/PracticeRxSwift/ViewController.swift
+++ FastCampus/PracticeRxSwift/PracticeRxSwift/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: Traits 알아보기
+// MARK: Traits 알아보기
         print("----Single1----")
         Single<String>.just("Check")
             .subscribe(
@@ -133,6 +133,95 @@ class ViewController: UIViewController {
             print($0)
         }
         .disposed(by: disposeBag)
+        
+// MARK: Subject 알아보기
+        print("---publishSubject---")
+        let publishSubject = PublishSubject<String>()
+        
+        publishSubject.onNext("1. 여러분 안녕하세요!")
+        
+        let subscriber1 = publishSubject
+            .subscribe(onNext: {
+                print($0)
+            }) // 자신이 구독을 시작한 이후에 생긴 event에 대해서만 결과 수용
+        
+        publishSubject.onNext("2. 들리세요?")
+        publishSubject.on(.next("3. 안들리시나요?"))
+        
+        subscriber1.dispose()
+        
+        let subscriber2 = publishSubject
+            .subscribe(onNext: {
+                print($0)
+            })
+        
+        publishSubject.onNext("4. 여보세요")
+        publishSubject.onCompleted()
+        
+        publishSubject.onNext("5. 끝났나요")
+        
+        subscriber2.dispose()
+        
+        publishSubject
+            .subscribe{
+                print("세번째 구독:", $0.element ?? $0)
+            }
+            .disposed(by: disposeBag)
+        
+        publishSubject.onNext("6. 찍힐까요?") // 이미 completed 된 publish subject
+        
+        print("---behaviorSubject---")
+        enum SubjectError: Error {
+            case error1
+        }
+        
+        let behaviorSubject = BehaviorSubject<String>(value: "0. 초기값")
+        
+        behaviorSubject.onNext("1. 첫번째 값")
+        
+        behaviorSubject.subscribe {
+            print("첫번째 구독:", $0.element ?? $0)
+        }
+        .disposed(by: disposeBag)
+        
+        behaviorSubject.onError(SubjectError.error1) // 에러를 발생시킴
+        
+        behaviorSubject.subscribe { // 직전에 발생한 이벤트 구독 (에러 포함)
+            print("두번째 구독:", $0.element ?? $0)
+        }
+        .disposed(by: disposeBag)
+        
+        // element의 값 뽑아내기
+        let value = try? behaviorSubject.value() // 가장 최신의 값을 꺼내줌
+        print(value!)
+        
+        print("---replaySubject---")
+        let replaySubject = ReplaySubject<String>.create(bufferSize: 2)
+        
+        replaySubject.onNext("1. 여러분")
+        replaySubject.onNext("2. 힘내세요")
+        replaySubject.onNext("3. 어렵지만") // bufferSize가 2라서 2.와 3.만 값을 받게 됨
+        
+        replaySubject.subscribe {
+            print("첫번째 구독:", $0.element ?? $0)
+        }
+        .disposed(by: disposeBag)
+        
+        replaySubject.subscribe {
+            print("두번째 구독:", $0.element ?? $0)
+        }
+        .disposed(by: disposeBag)
+        
+        replaySubject.onNext("4. 할 수 있어요")
+        replaySubject.onError(SubjectError.error1)
+        replaySubject.dispose()
+        
+        replaySubject.subscribe { // 이미 dispose된 observer를 구독하려 하므로 error
+            print("세번째 구독:", $0.element ?? $0)
+        }
+        .disposed(by: disposeBag)
+        
+// MARK: Filtering Operator
     }
 }
 
