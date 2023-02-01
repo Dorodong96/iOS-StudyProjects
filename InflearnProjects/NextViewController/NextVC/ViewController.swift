750fe4dbd7a4f79648ddd608ad5772315b6a77c8
Dongkyu Kim <dk143496@gmail.com>
2023-02-01 23:43:12 +0900

    Update ViewController.swift

--- InflearnProjects/NextViewController/NextVC/ViewController.swift
+++ InflearnProjects/NextViewController/NextVC/ViewController.swift
@@ -30,6 +30,11 @@ class ViewController: UIViewController {
         
     }
     
+    // secondVC.mainLabel.text = "전달하려는 데이터"
+    // 이런식으로 전달하면 Runtime Error 발생
+    // 코드베이스는 각 인스턴스가 컴파일타임에 메모리에 올라감
+    // 스토리보드 인스턴스가 이후에 연결되기 때문에, 연결되기 전에 호출되어서 에러
+    // viewDidLoad가 스토리보드가 코드와 연결됨
     
     
     // 3) 스토리보드에서의 화면 이동(간접 세그웨이)
